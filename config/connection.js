35287f9aa41424e17f7b95b9b507d07944c5faee
Mark Edwards <109709559+mark-227-g@users.noreply.github.com>
2023-06-12 18:14:02 -0400

    added environment for heroku

--- config/connection.js
+++ config/connection.js
@@ -11,9 +11,14 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
+      /*
       host: '127.0.0.1',
       dialect: 'mysql',
       port: 3306
+      */
+     host: process.env.DB_HOST,//process.env.DB_HOST,
+     dialect: process.env.DB_CONNECTION,
+     port: process.env.DB_PORT
     }
   );
 }
